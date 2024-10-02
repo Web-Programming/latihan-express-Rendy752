2c74477fdf04005fe0cb998f1b48e72dae731252
Rendy Pratama <rendypratama752@gmail.com>
2024-10-02 09:32:18 +0700

    add: latihan-express

---
+++ latihan-express/routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Beranda' });
+});
+
+router.get('/about', function (req, res, next) {
+  res.render('about', { title: 'Tentang Kami' });
+});
+
+router.get('/kontak', function (req, res, next) {
+  res.render('contact', {
+    title: 'Hubungi Kami',
+    name: 'Rendy Pratama',
+    email: 'rendypratama752@gmail.com',
+    message: 'Silahkan hubungi kami melalui email',
+  });
+});
+
+module.exports = router;
