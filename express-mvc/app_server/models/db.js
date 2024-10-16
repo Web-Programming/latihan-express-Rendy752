d4f2736210d7efc1c08199e5ad0dd1a2ad9ad051
Rendy Pratama <rendypratama752@gmail.com>
2024-10-16 09:25:37 +0700

    add: mongodb connection

---
+++ express-mvc/app_server/models/db.js
@@ -0,0 +1,18 @@
+let mongoose = require('mongoose');
+let dbURI = 'mongodb://localhost:27017/rendyp';
+
+mongoose.connect(dbURI, {
+  // useNewUrlParser: true
+});
+
+mongoose.connection.on('connected', () => {
+  console.log(`Mongoose connected to ${dbURI}`);
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log('Mongoose connection error:', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected');
+});
